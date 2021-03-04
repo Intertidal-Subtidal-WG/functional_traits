038296a2786f2a1d3864c5f8dad17f99705fdd06
Kate Sheridan <39718329+Lithobius@users.noreply.github.com>
2021-03-03 19:17:20 -0500

    more subsets and filters
    refining the list of species that may be added to the traits document

--- scripts/ks_intertidal_list.R
+++ scripts/ks_intertidal_list.R
@@ -27,9 +27,36 @@ ct_top <- ct_sp %>% filter(ct_sp %in% top_intertidal$name) %>%
 pc_top <- pc_sp %>% filter(pc_sp %in% top_intertidal$name) %>%
   rename(Organism = pc_sp)
 
-#merge
+#merge0 
 inter_top <- merge(ct_top, pc_top, all.x = TRUE, all.y = TRUE)
 
 #remove things that aren't plants or animals...
 not_alive <- c('Bare rock', 'Black zone', 'Shell hash')
 inter_top <- subset(inter_top, !(inter_top$Organism %in% not_alive))
+
+#remove species we already have in our sheet
+in_sheet <- c('Saccorhiza dermatodea','Saccharina latissima','Mytilus edulis',
+  'Modiolus modiolus','Alaria esculenta','Strongylocentrotus droebachiensis',
+  'Henricia sanguinolenta','Colpomenia peregrina','Codium fragile',
+  'Cancer irroratus','Cancer borealis','Asterias rubens','Asterias forbesi',
+  'Ascophylum nodosum','Schizoporella unicornis','Ptilota serrata','Porphyra',
+  'Polysiphonia','Palmaria palmata','Ostrea edulis','Membranipora membranacea',
+  'Mastocarpus stellatus','Lithophyllum ','Hildenbradia rubra',
+  'Euthora cristata','Didemnum vexillum','Cystoclonium purpureum',
+  'Crepidula fornicata','Corallina officinalis','Clathromorphum circumscriptum',
+  'Chondrus crispus','Botryllus schlosseri','Botryllus violaceus',
+  'Bonnemaisonia hamifera')
+
+inter_unique <- subset(inter_top, !(inter_top$Organism %in% in_sheet))
+
+ #of these lets sort out the ct and pc data with only these and >0 count
+
+ct_filter <- ct_data %>% filter(Organism %in% inter_top$Organism & Count > 0
+                                & Transect %in% transect_choice)
+
+pc_filter <- pc_data %>% filter(Organism %in% inter_top$Organism & Percent_cover > 0
+                                & Transect %in% transect_choice)
+
+
+
+
