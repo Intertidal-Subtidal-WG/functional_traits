787df9ab5e6124ddf9d6c6e7e62fd8f738975978
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2021-09-15 16:29:20 -0600

    add count and cover datasets and start summarizing count data in the script

--- scripts/plotting_trait_ts.R
+++ scripts/plotting_trait_ts.R
@@ -4,10 +4,36 @@
 # PACKAGES and DATA 
 library(tidyverse)
 
-data <- readRDS("data/fielddata/combined_intertidal_abundance.RDS")
+count_data <- read_csv("data/fielddata/ct_clean.csv")
+cover_data <- read_csv("data/fielddata/pc_clean.csv")
 
 # SUMMARIZE
 
-cover <- data %>% 
-  select(SITE, YEAR, INTERTIDAL_TRANSECT, LEVEL, REPLICATE, DATA_TAKEN, ORGANISM,
-         VALUE, MEASURE, VALID_NAME)
+# average any replicates and remove rows where no data was collected
+count <-  count_data %>% 
+  select(Year, Transect, Level, Replicate, Data_taken, Organism, Count) %>% 
+  filter(Data_taken != "no") %>%
+  group_by(Year, Transect, Level, Replicate, Organism) %>% 
+  summarise(Count = mean(Count)) %>% 
+  ungroup() %>% 
+  select(-Replicate)
+
+# get average count per plot for each transect (by year)
+count_avg_by_transect <- count %>%
+  group_by(Year, Transect, Organism) %>%
+  summarise(Abund = sum(Count)) %>% 
+  left_join(., count %>% 
+              group_by(Year, Transect) %>% 
+              summarise(n_level = n_distinct(Level))) %>% 
+  mutate(Avg_abund_per_level = Abund / n_level)
+
+# get average count per plot/transect per year
+count_avg_by_year <- count %>% 
+  group_by(Year, Organism) %>% 
+  summarise(Abund = sum(Count)) %>% 
+  left_join(., count %>% group_by(Year) %>% 
+              summarise(n_transect = n_distinct(Transect))) %>% 
+  mutate(Avg_abund_per_transect = Abund / n_transect)
+  
+
+
